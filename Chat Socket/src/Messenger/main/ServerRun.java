0999aa0a2a787aa5c8de9eef1fad34ba2b7eb961
Marry <mantm.22ite@vku.udn.vn>
2023-07-10 18:16:48 +0700

    up

---
+++ Chat Socket/src/Messenger/main/ServerRun.java
@@ -0,0 +1,9 @@
+package Messenger.main;
+import Messenger.control.*;
+public class ServerRun {
+
+    public static void main(String[] args) {
+        ServerControl controller = new ServerControl(1234, 5, "serverRMI", 1099);
+        controller.listening();
+    }
+}
---
+++ Chat Socket/src/finalLab/main/ClientRun.java
@@ -0,0 +1,13 @@
+package finalLab.main;
+import finalLab.control.ClientControl;
+import finalLab.view.ClientView;
+
+public class ClientRun {
+
+    public static void main(String[] args) {
+        ClientView view = new ClientView();
+        ClientControl control = new ClientControl(view);
+        view.setVisible(true);
+    }
+
+}
